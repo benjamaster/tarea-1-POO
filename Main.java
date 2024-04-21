d65fe9030c1881997b9c5855140909f161696a12
Niell06 <167658567+Niell06@users.noreply.github.com>
2024-04-21 18:49:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) throws IOException{
         // Carga de archivo config.csv
+        Inventario inventario = new Inventario();
         if (args.length != 1) {
             System.out.println("Usage: java Stage1 <config.csv>");
             //System.exit(-1);
@@ -16,18 +17,17 @@ public class Main{
         Scanner in = new Scanner(new File("config.csv"));
         Main stage1 = new Main();
         // Lectura de archivo config.csv
-        stage1.readConfiguration(in);
+        stage1.readConfiguration(in, inventario);
 
-        stage1.executeAction(new Scanner(System.in), System.out);
+        stage1.executeAction(new Scanner(System.in), System.out, inventario);
         System.out.println("Programa terminado");
     }
 
-    public void readConfiguration(Scanner in){
+    public void readConfiguration(Scanner in,  Inventario inventario){
         // Creación de mascota
         String nombre_mascota = in.nextLine();
         mascota = new Mascota(nombre_mascota);
         // Creación de inventario vacío
-        Inventario inventario = new Inventario();
         // Llenando inventario
         while (in.hasNextLine()) {
             String linea = in.nextLine();
@@ -42,12 +42,26 @@ public class Main{
             que corresponda */
             // Se realiza inicializacion Stage 3 -> 
             // se inicializa los 4 items de prueba
-            inventario.addItem(1,Juguete,Pelota,4);
-            inventario.addItem(2,Comida,Queso,5);
-            inventario.addItem(3,Comida,Pan,3);
-            inventario.addItem(4,Medicina,Jarabe,4);
+           /*  inventario.addInv(1,"Juguete","Pelota",4);
+            inventario.addInv(2,"Comida","Queso",5);
+            inventario.addInv(3,"Comida","Pan",3);
+            inventario.addInv(4,"Medicina","Jarabe",4);*/
+
         }
 
+        // Crear algunos elementos
+        Item item1 = new Juguete("Pelota", 1, 4);
+        Item item2 = new Comida("Queso", 2, 5);
+        Item item3 = new Comida("Pan", 3, 3);
+        Item item4 = new Medicina("Jarabe", 4, 4);
+
+        // Agregar los elementos al inventario
+        inventario.addItem(item1);
+        inventario.addItem(item2);
+        inventario.addItem(item3);
+        inventario.addItem(item4);
+
+        /* 
         System.out.println("Mascota Virtual\n");
         System.out.println("'\nAtributos\n---------\nNombre: " + mascota.Nombre +  "\nEdad:" + mascota.Edad + "\nSalud:" + mascota.Salud + "\nEnergía:" + mascota.Energia + "\nFelicidad:" + mascota.Felicidad + "\nEstado:" + mascota.getEstado().getMensaje());
         mascota.addFelicidad(50);
@@ -56,9 +70,15 @@ public class Main{
         System.out.println("'\nAtributos\n---------\nNombre: " + mascota.Nombre +  "\nEdad:" + mascota.Edad + "\nSalud:" + mascota.Salud + "\nEnergía:" + mascota.Energia + "\nFelicidad:" + mascota.Felicidad + "\nEstado:" + mascota.getEstado().getMensaje());
         mascota.addEnergia(150);
         System.out.println("'\nAtributos\n---------\nNombre: " + mascota.Nombre +  "\nEdad:" + mascota.Edad + "\nSalud:" + mascota.Salud + "\nEnergía:" + mascota.Energia + "\nFelicidad:" + mascota.Felicidad + "\nEstado:" + mascota.getEstado().getMensaje());
+        */
     }
     
-    public void executeAction(Scanner in, PrintStream out){
+    /**
+     * @param in
+     * @param out
+     * @param inventario
+     */
+    public void executeAction(Scanner in, PrintStream out, Inventario inventario){
         /* Completar código con manejo de acciones y menú en las etapas que corresponda
         e incremento del tiempo dependiendo de la etapa */
         Scanner scan = new Scanner(System.in);
@@ -67,31 +87,49 @@ public class Main{
         System.out.println("Test Mascota Virtual\n");
         do {
             System.out.println("Tiempo Simulado:" + Time);
-            System.out.println("'\nAtributos\n---------\nNombre: " + mascota.Nombre +  "\nEdad:" + mascota.Edad + "\nSalud:" + mascota.Salud + "\nEnergía:" + mascota.Energia + "\nFelicidad:" + mascota.Felicidad + "\nEstado:" + mascota.getEstado().getMensaje());
+            System.out.println("\nAtributos\n---------\nNombre: " + mascota.Nombre +  "\nEdad:" + mascota.Edad + "\nSalud:" + mascota.Salud + "\nEnergía:" + mascota.Energia + "\nFelicidad:" + mascota.Felicidad + "\nEstado:" + mascota.getEstado().getMensaje());
             System.out.println("\nAcciones---------\n");
-            inventario.imprimirInventario();
-             System.out.println("\nSeleccione un elemento del inventario:\n");
+            inventario.imprimir_inventario();
+            System.out.println("\nSeleccione un elemento del inventario:");
             opcion = scan.nextInt();
 
             switch(opcion){
                 case 1:
                     //Ocupar item 1
+                    Item itemSeleccionado = inventario.getItems().get(opcion - 1);
+                    itemSeleccionado.usar_item(mascota);
+                    //itemSeleccionado.printItemAplicado();//Arreglar Sigue apareciendo que juega a pesar de no estar disponible
                     break;
                 case 2: 
                     //Ocupar item 2
+                    Item itemSeleccionado2 = inventario.getItems().get(opcion - 1);
+                    itemSeleccionado2.usar_item(mascota);
+                    //itemSeleccionado2.printItemAplicado();
                     break;
                 case 3:
                     //Ocupar item 3
+                    Item itemSeleccionado3 = inventario.getItems().get(opcion - 1);
+                    itemSeleccionado3.usar_item(mascota);
+                   // itemSeleccionado3.printItemAplicado();
                     break;
                 case 4:
                     //Ocupar item 4
+                    Item itemSeleccionado5 = inventario.getItems().get(opcion - 1);
+                    itemSeleccionado5.usar_item(mascota);
+                    //itemSeleccionado5.printItemAplicado();
                     break;
             }
-            mascota.addEdad(0.5);
-            Time = Time + 0.5;          
+            mascota.addEdad(0.5f);
+            Time = Time + 0.5f;
+            mascota.addEnergia(-5);
+            mascota.addSalud(-5);   
+            mascota.addFelicidad(-5); 
+
                           
             
         } while(mascota.Edad < 15 || mascota.Salud > 0 || mascota.Energia > 0);
+
+        scan.close(); //cerrar instancia scan despues de utilizar
     
     }
 
@@ -102,5 +140,5 @@ public class Main{
     }
 
     private Mascota mascota;
-    // private Inventario inventario; // Descomentar cuando se haya creado el inventario
+    //private Inventario inventario; // Descomentar cuando se haya creado el inventario
 }
